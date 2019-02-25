fd44907d3810410a2f732ac14aa955a7078e637b
angelovangel <aangeloo@gmail.com>
2019-02-25 21:03:17 +0100

    new version

--- R/readGC.R
+++ R/readGC.R
@@ -2,16 +2,21 @@
 #' 
 #' Parser for Shimadzu GCsolution and GCMSsolution data
 #' 
-#' @param file A text file, exported from the Shimadzu GCsolution or GCMS solution. 
+#' @param file a text file, exported from the Shimadzu GCsolution or GCMS solution. 
 #' Only the "Compound Quantitative Result" must be clicked when exporting.
-#' @param ncompounds Integer. Number of compounds, as defined in the compound table. Future versions will 
-#' detect this automatically.
+#  @param ncompounds Integer. Number of compounds, as defined in the compound table. Future versions will 
+#  detect this automatically.
+#' @param writexl logical: write the resulting tibble to xlsx?
+#' 
+#' @usage readGC(file, writexl = FALSE)
 #' 
 #' @return A tibble containing all the information from the exported file.
 #' The \code{sample} variable contains sample names, generated from the file names 
-#' (matches word/dot/dash/space from the end of file name and removes the .gcd or .qgd extension).
+#' (matches word/dot/dash/space from the end of file name and removes the .gcd or .qgd extension). 
+#' The function tries to detect the number of samples and compounds and prints this information. If the \code{writexl} 
+#' argument is \code{TRUE}, then an excel file with the data is written to the current directory.
 #' 
-#' @importFrom stringr str_extract str_remove str_split
+#' @importFrom stringr str_extract str_remove str_split str_detect
 #' @importFrom dplyr as_tibble '%>%'
 #' @importFrom utils read.table
 #' 
@@ -21,18 +26,40 @@
 
  #library(tidyverse)
 
- readGC <- function(file, ncompounds) {
+ readGC <- function(file, writexl = FALSE) {
   flines <- readLines(file)
   # first remove those tabs at the end #
   flines <- str_remove(flines, "\\t$")
   
+  #### detect number of samples ####
   n <- as.integer(grep("[Header]", flines, fixed = TRUE) %>% length()) # number of runs 
   cat(n, " samples were detected...", "\n")
+  ####
   
-  skip <- seq(8, length.out = n, by = ncompounds + 9) # tell which lines are just before the data
+  # test if I can detect ncompounds
+  nIDs <- flines %>% 
+    str_detect(pattern = "# of IDs") %>% 
+    subset(flines, subset = .) %>% 
+    str_split(pattern = "\t") %>% 
+    do.call(rbind, .) %>% .[ , 2] %>% as.numeric()
+  
+  if(length(unique(nIDs)) != 1) {
+    stop("All samples must have equal number of compounds! Check your batch processing...")
+  }
+  
+  ncompounds2 <- nIDs[1]
+  cat(ncompounds2, " compounds were detected...", "\n")
+  
+  if(length(nIDs) != n) {
+    stop("Something went wrong, check that your exported file has the same structure as the examples.")
+  }
+  ###################
+  
+  ### define 
+  skip <- seq(8, length.out = n, by = ncompounds2 + 9) # tell which lines are just before the data
   
   # subset data only, first get indices of data lines
-  dataidxs <- lapply(1:ncompounds, function(i) {skip + i})
+  dataidxs <- lapply(1:ncompounds2, function(i) {skip + i})
   # then make one vector of indices and use it to subset the lines
   dataidx <- c(do.call(rbind, dataidxs))
   
@@ -44,8 +71,10 @@
   names <- flines[grep("Data File Name", flines)] %>%
            # match word or dot or dash or space!, positive lookahead end of line
            str_extract(pattern = "([ \\w.-]+)(?=$)") %>%
+           # remove file extensions
+           str_remove(pattern = "(.gcd|.qgd)") %>%
            # repeat for each compound, note the "each" argument
-           rep(each = ncompounds)
+           rep(each = ncompounds2)
            
   
   #check before adding to df
@@ -54,5 +83,16 @@
     }
   
   df[["sample"]] <- names
+  
+  #### write xlsx if required ####
+  if(isTRUE(writexl)){
+      if(!requireNamespace(package = "writexl", quietly = TRUE)) {
+      stop("Please install \"writexl\" in order to write xlsx files. You can do it with install.packages(\"writexl\")")
+      }
+    basefilename <- basename(file) %>% str_split(pattern = "\\.") %>% unlist() %>% .[1]
+    writexl::write_xlsx(df, path = paste0(basefilename, ".xlsx"))
+    cat(basefilename, ".xlsx was written to current directory", "\n", sep = "")
+  }
+  
   return(as_tibble(df))
  }
